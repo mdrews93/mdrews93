84315141c3faea3cea669b85b6dc69828cf144e4
mdrews <mdrews@hawk.iit.edu>
2017-05-03 14:29:25 -0500

    Added second blog post, updated site theme.

--- publishconf.py
+++ publishconf.py
@@ -22,3 +22,31 @@ DELETE_OUTPUT_DIRECTORY = True
 
 #DISQUS_SITENAME = ""
 #GOOGLE_ANALYTICS = ""
+
+AUTHOR = 'Michael Drews'
+SITENAME = "Michael's Blog"
+
+PATH = 'content'
+
+TIMEZONE = 'America/Chicago'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+MARKUP = ('md', 'ipynb')
+
+PLUGIN_PATH = './plugins'
+PLUGINS = ['ipynb.markup']
+
+THEME = 'C:/users/mdrews/Documents/pelican-themes/gum'
